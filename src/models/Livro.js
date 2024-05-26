e8213f7050d4b3318243b9e27c2a235d089f7522
Brendhalorranyr <Brendhak200@gmail.com>
2024-05-26 15:06:07 -0300

    Feat: Criação do arquivo de pontapé do servidor e da classe Livro

---
+++ src/models/Livro.js
@@ -0,0 +1,9 @@
+export class Livro {
+    constructor(titulo, autor, genero, imagem, quantidade) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.imagem = imagem;
+        this.quantidade = quantidade;
+    }
+}
